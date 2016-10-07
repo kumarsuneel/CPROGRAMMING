1d0d6c6330e8b87b9bde55910e44dbfa71539200
Suneel Dogra <kumar_sunil@live.com>
2016-10-07 09:21:41 +0530

    Create arraystring.c

---
+++ arraystring.c
@@ -0,0 +1,16 @@
+//Array of String 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char str[20]={'S','u','n','i','l','\0'};
+    int i;
+    for(i=0;i[str]!='\0';i++)
+    {
+        printf("%c",str[i]);
+
+    }
+     return 0;
+}
